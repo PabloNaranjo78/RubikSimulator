06c2d3a6765a9ca69616926433968ac5b0aa2860
Axel Cordero <axeljcm@estudiantec.cr>
2022-03-07 15:44:55 -0600

    Verificacion de condiciones Columnas y filas

--- RS.rkt
+++ RS.rkt
@@ -2,74 +2,42 @@
 
 (require "Utilidades.rkt")
 
-;(define (nth-column M n)
-  ;(map (lambda (row) (list-ref row n)) M))
-
-;(define (nth-row M n)
-  ;(map (lambda (column) (list-ref column n)) M))
-
-;(define M '((1 2 3)
-            ;(2 3 4)
-           ; (6 7 9)))
-
-;(nth-column M 1)
-
-;(nth-row M 0)
-
-;(nth-column M 0)
-
-;(nth-column M 2)
-
-
 (define (RS Tam Matriz Movs)
   (cond
     ;Condicion para verificar correcta cantidad de caras (6)
-    ((and (equal? (largo Matriz) 6) (lenFil Tam Matriz 1 1)) (display "ciclo terminado")) ;(lenCol Tam Matriz)) "Funcion de Movs"))
-          (else
-           (display "error"))))
+    ((and (equal? (largo Matriz) 6)  (lenFilCol Tam Matriz 1 1))  (display "ciclo terminado"))  ;"Funcion de Movs"))
+    (else
+     (display "error"))))
 
-     (define (lenFil Tam Matriz cont1 cont2)
-       (cond
-         ((> cont2 6) #t)
-         (else (lenFil-aux Tam Matriz cont1 cont2))))
+(define (lenFilCol Tam Matriz cont cont2)
+  (cond
+    ((> (largo(elemento cont Matriz)) Tam) #f)
+    ((= cont2 6) #t)
+    ((> cont Tam) (lenFilCol-aux Tam Matriz 1 (+ 1 cont2)))
+    (else (lenFilCol-aux Tam Matriz cont cont2))))
     
 
-     (define (lenFil-aux Tam Matriz cont1 cont2)
-       (cond
-         ((equal? Tam (largo '(3 3))) (lenFil-aux Tam Matriz (+ 1 cont1) (+ 1 cont2 )))
-                                                                    (else 
-                                                                     (#f))))
+(define (lenFilCol-aux Tam Matriz cont cont2)
+  (cond
+    ((equal? (largo(elemento cont (elemento cont2 Matriz))) Tam) (lenFilCol Tam Matriz (+ 1 cont ) cont2))
+  (else #f)))
+
    
-
 ;(largo (elemento cont1 (elemento 1 Matriz)))        
-;(define (lenCol Tam Matriz) 
-;(cond
- ; [(empty? (elemento2 0 Matriz)) 0]
- ; [else (+ (elemento2 0 Matriz) ( lenCol(rest '(elemento2 0 Matriz))))]))
+;(define (lenCol Tam Matriz cont) 
+ ; (cond
+ ;   [(empty? (elemento2 0 Matriz)) #t]
+  ;  [else (+ (elemento2 0 Matriz) ( lenCol(rest '(elemento2 0 Matriz))))]))
                                                                                        
-;(define (elemento index lista)
- ; (cond ((null? lista)
-     ;    #f)
-     ;   ((equal? index 1)
-      ;   (car lista))
-      ;   (else
-       ;   (elemento (- index 1)(cdr lista)))))
 
-;(define (elemento2 index elemento)
-  ;(cond ((null? elemento)
-      ;   #f)
-       ; ((equal? index 1)
-      ;   (car elemento))
-      ;   (else
-       ;   (elemento2 (- index 1)(cdr elemento)))))
 
-;(define (movimiento last Matriz)
-  ;(cond
-    ;((if length last Matriz) 0)
+
+
+
 
 (RS  3 '( ( (A A A)
             (A A A)
-            (A A A) )
+            (A A A))
 
           ( (B B B)
             (B B B)
