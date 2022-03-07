adaadda0413242294b682196e6167d12437364ba
PabloNaranjo78 <pablonaranjo5@estudiantec.cr>
2022-03-07 12:34:22 -0600

    Se puede pintar los lados

--- GUI.rkt
+++ GUI.rkt
@@ -1,14 +1,40 @@
 #lang scheme
 (require (lib "graphics.ss" "graphics"))
+(require )
 (provide iniciar)
 
 
 (open-graphics)
 
+(define (obtnerColor color)
+  (cond ((equal? color 'A)
+         "blue")
+         ((equal? color 'B)
+          "white"
+        )
+         ((equal? color 'V)
+          "green"
+        )
+         ((equal? color 'Y)
+          "yellow"
+        )
+         ((equal? color 'N)
+          "orange"
+        )
+         ((equal? color 'R)
+          "red"
+        )
+         ((equal? color 'Q)
+          "black"
+        )(else
+          "magenta")
+         ))
+
+
 ;Vértices de cubo
 
 (define aX 253)
-(define aY 150)
+(define aY 143)
 
 (define bX 472)
 (define bY 38)
@@ -28,18 +54,59 @@
 (define gX 253)
 (define gY 471)
 
-(define (iniciar num)
-  (display "iniciando")
+(define (iniciar num nn)
+ ; (println "iniciando")
+   
+   (pintarCaraArribaLado "green" num aX aY bX bY cX cY dX dY)
+   (pintarCarafrente "yellow" num aX aY dX dY fX fY gX gY)
+   (pintarCaraArribaLado "red" num dX dY cX cY eX eY fX fY)
+  (repintarLineas num)
+  )
+
+(define (pintarCarafrente lista num arista1X arista1Y arista2X arista2Y arista3X arista3Y arista4X arista4Y)
+
+  (for ([k (in-range arista1X (* num arista2X) (/(- arista4X arista1X) num))]
+        [m (in-range arista1Y (* num arista4Y) (/(- arista4Y arista1Y) num))]
+        [o (in-range 0 num 1)] #:break (= o num))  
+   (for ([i (in-range k (* num arista2X) (/(- arista2X arista1X) num))]
+         [j (in-range m (* num arista2Y) (/(- arista2Y arista1Y) num))]
+         [n (in-range 0 num 1)] #:break (= n num) )
+    ((draw-solid-polygon ventana) (list
+                                   (make-posn i j)
+                                   (make-posn (+ i (/ (- arista2X arista1X) num)) (+ j (/ (- arista2Y arista1Y) num))) 
+                                   (make-posn (+ (/ (- arista4X arista1X) num) (+ i (/ (- arista2X arista1X) num))) (+ (/ (- arista4Y arista1Y) num) (+ j (/ (- arista2Y arista1Y) num))))
+                                   (make-posn (+ i (/ (- arista4X arista1X) num)) (+ j (/ (- arista4Y arista1Y) num)))) (make-posn 0 0) lista)
+     )
+    )
+    )
+
+(define (pintarCaraArribaLado lista num arista1X arista1Y arista2X arista2Y arista3X arista3Y arista4X arista4Y)
+
+  (for ([k (in-range arista1X (* num arista2X) (/(- arista4X arista1X) num))]
+        [m (in-range arista1Y (* num arista4Y) (/(- arista4Y arista1Y) num))]
+        [o (in-range 0 num 1)] #:break (= o num))  
+   (for ([i (in-range k (* num arista2X) (/(- arista2X arista1X) num))]
+         [j (in-range m arista2Y (/(- arista2Y arista1Y) num))]
+         [n (in-range 0 num 1)] #:break (= n num) ) 
+    ((draw-solid-polygon ventana) (list
+                                   (make-posn i j)
+                                   (make-posn (+ i (/ (- arista2X arista1X) num)) (+ j (/ (- arista2Y arista1Y) num))) 
+                                   (make-posn (+ (/ (- arista4X arista1X) num) (+ i (/ (- arista2X arista1X) num))) (+ (/ (- arista4Y arista1Y) num) (+ j (/ (- arista2Y arista1Y) num))))
+                                   (make-posn (+ i (/ (- arista4X arista1X) num)) (+ j (/ (- arista4Y arista1Y) num)))) (make-posn 0 0) lista)
+     )
+    )
+    )   
+
+
+
+
+(define (repintarLineas num)
   (crearLineas num aX aY bX bY cX cY dX dY)
   (crearLineas num dX dY cX cY eX eY fX fY)
   (crearLineas num aX aY dX dY fX fY gX gY)
-
   (crearLineas num dX dY aX aY bX bY cX cY)
   (crearLineas num fX fY dX dY cX cY eX eY)
-  (crearLineas num gX gY aX aY dX dY fX fY)
-  
-  )
-
+  (crearLineas num gX gY aX aY dX dY fX fY))
 
 (define ventana (open-viewport "Rubik Simulator" 1000 600))
 
@@ -48,7 +115,6 @@
         [j (in-range arista2Y (+ (/ (/ (- arista4Y arista1Y) cant) 3) arista3Y) (/ (- arista3Y arista2Y) cant))]
         [k (in-range arista1X (+ 1 arista4X) (/ (- arista4X arista1X) cant))]
         [m (in-range arista2X (+ 1 arista3X) (/ (- arista3X arista2X) cant))])
-    (sleep 0.05)
     ((draw-solid-polygon ventana) (list
                                    (make-posn (- k 2) (- i 2))
                                    (make-posn (- m 2) (- j 2))
@@ -56,4 +122,4 @@
                                    (make-posn (+ k 2) (+ i 2))) (make-posn 0 0) "black"))
 )
 
-(iniciar 30)
+(iniciar 6 2)
